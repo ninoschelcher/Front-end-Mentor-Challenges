d724abb25259a6088cdd53bbc30767ad627193ec
Nino Schelcher <onin1998@gmail.com>
2020-02-09 12:11:57 +0100

    start

---
+++ manage-landing-page-master/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  important: true,
+  theme: {
+    extend: {
+      colors: {
+        'bright-red': '#F25F3A',
+        'dark-blue': '#242D52',
+        'dark-greyish-blue': '#9095A7',
+        'very-dark-blue': '#1D1F25',
+        'very-pale-red': '#FFEFEB',
+        'very-light-blue': '#FAFAFA',
+      },
+    }
+  },
+}
